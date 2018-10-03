cbab6eaa6596033cc2d5b98cc5442817fc0ae7ad
Akash Mittal <akash.mittal@irissoftware.com>
2018-10-03 09:37:37 +0530

    Changes:1.) Removed Unused Classes.

--- bp-wallet-client/src/main/java/com/betpawa/wallet/client/controller/WalletClientController.java
+++ bp-wallet-client/src/main/java/com/betpawa/wallet/client/controller/WalletClientController.java
@@ -17,21 +17,20 @@ import com.betpawa.wallet.client.service.WalletClientService;
 @RestController
 @RequestMapping("/")
 public class WalletClientController {
-    private static final Logger logger = LoggerFactory.getLogger(WalletClientController.class);
+	private static final Logger logger = LoggerFactory.getLogger(WalletClientController.class);
 
-    @Autowired
-    private WalletClientService walletClientService;
+	@Autowired
+	private WalletClientService walletClientService;
 
-    @PostMapping
-    public WalletClientResponse execute(@RequestBody WalletClientRequest walletClientRequest)
-            throws InterruptedException {
-        long time = System.currentTimeMillis();
-        WalletClientResponse response = new WalletClientResponse.Builder()
-                .transactions(walletClientService.run(walletClientRequest)).build();
-        long timeTaken = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - time);
-        logger.info("Excecution done time taken: {} {}", timeTaken, TimeUnit.SECONDS.name());
-        response.setTimeTaken(timeTaken);
-
-        return response;
-    }
+	@PostMapping
+	public WalletClientResponse execute(@RequestBody WalletClientRequest walletClientRequest)
+			throws InterruptedException {
+		long time = System.currentTimeMillis();
+		WalletClientResponse response = new WalletClientResponse.Builder()
+				.transactions(walletClientService.run(walletClientRequest)).build();
+		long timeTaken = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - time);
+		logger.info("Excecution done time taken: {} {}", timeTaken, TimeUnit.SECONDS.name());
+		response.setTimeTaken(timeTaken);
+		return response;
+	}
 }
