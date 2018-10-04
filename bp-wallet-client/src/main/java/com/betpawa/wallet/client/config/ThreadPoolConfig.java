68b5cb004234bd8b95661cfbd9ac7ae3c8e53315
Akash Mittal <akash.mittal@irissoftware.com>
2018-10-04 10:08:30 +0530

    Changes:1.) Minor Fixes.

--- bp-wallet-client/src/main/java/com/betpawa/wallet/client/config/ThreadPoolConfig.java
+++ bp-wallet-client/src/main/java/com/betpawa/wallet/client/config/ThreadPoolConfig.java
@@ -1,8 +1,5 @@
 package com.betpawa.wallet.client.config;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
@@ -10,20 +7,15 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 public class ThreadPoolConfig {
-    @Bean
-    public TaskExecutor threadPoolTaskExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(10);
-        executor.setMaxPoolSize(10);
-        executor.setQueueCapacity(500);
-        executor.setThreadNamePrefix("bp-wallet-client");
-        executor.initialize();
-        return executor;
-    }
-
-    @Bean
-    ExecutorService executorService() {
-        return Executors.newFixedThreadPool(10);
-    }
+	@Bean
+	public TaskExecutor threadPoolTaskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(10);
+		executor.setMaxPoolSize(10);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("bp-wallet-client");
+		executor.initialize();
+		return executor;
+	}
 
 }
